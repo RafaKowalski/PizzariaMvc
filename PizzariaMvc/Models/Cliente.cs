ec06ecc9af8b0122a684459e41a35a53394a876a
Rafael Kowalski <rafaelmk06@gmail.com>
2023-01-03 16:10:59 -0300

    Adicionando migração para criação da tabela de clientes, criado o seu controller e sua view de novo cliente

---
+++ PizzariaMvc/Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿namespace PizzariaMvc.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public int NumeroEndereco { get; set; }
+        public string Complemento { get; set; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(int id, string nome, string telefone, string endereco, int numeroEndereco, string complemento)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+            NumeroEndereco = numeroEndereco;
+            Complemento = complemento;
+        }
+    }
+}
