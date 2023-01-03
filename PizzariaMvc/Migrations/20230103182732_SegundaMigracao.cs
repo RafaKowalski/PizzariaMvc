404f1d14fa650b15219e3df2b158179c326b8124
Rafael Kowalski <rafaelmk06@gmail.com>
2023-01-03 15:28:26 -0300

    Realizada segunda migração para corrigir campos da model Pizza

---
+++ PizzariaMvc/Migrations/20230103182732_SegundaMigracao.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzariaMvc.Migrations
+{
+    public partial class SegundaMigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TamanhoPizza",
+                table: "Pizzas");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Pizzas",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Preco",
+                table: "Pizzas",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Pizzas");
+
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "Pizzas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TamanhoPizza",
+                table: "Pizzas",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
