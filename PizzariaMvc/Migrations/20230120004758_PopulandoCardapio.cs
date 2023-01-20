f6a424fffa9715a0e093c1b8f2802daee52222a6
Rafael Kowalski <rafaelmk06@gmail.com>
2023-01-19 21:57:53 -0300

    Populando tabela de cardapio

---
+++ PizzariaMvc/Migrations/20230120004758_PopulandoCardapio.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzariaMvc.Migrations
+{
+    public partial class PopulandoCardapio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Cardapios(Sabor,Descricao)" + "Values('Calabresa','Pizza de Calabresa com queijo muzzarela e molho de tomate')");
+
+            migrationBuilder.Sql("Insert into Cardapios(Sabor,Descricao)" + "Values('Coração','Pizza de Coração com queijo muzzarela e molho de tomate')");
+
+            migrationBuilder.Sql("Insert into Cardapios(Sabor,Descricao)" + "Values('Muzzarela','Pizza de queijo muzzarela e molho de tomate')");
+
+            migrationBuilder.Sql("Insert into Cardapios(Sabor,Descricao)" + "Values('Frango catupiry','Pizza de Frango desfiado com catupiry, queijo muzzarela e molho de tomate')");
+
+            migrationBuilder.Sql("Insert into Cardapios(Sabor,Descricao)" + "Values('Filé com palhas','Pizza de filé mignom com batata palha, queijo muzzarela e molho de tomate')");
+
+            migrationBuilder.Sql("Insert into Cardapios(Sabor,Descricao)" + "Values('Rúcula com tomate seco','Pizza de Rúcula com tomate seco, queijo muzzarela e molho de tomate')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Cardapios");
+        }
+    }
+}
